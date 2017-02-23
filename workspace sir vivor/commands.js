c0a88eb7fd769e5941a2ba6f54963f020768f58a
CheeseMuffin <meesechuffin@gmail.com>
2017-02-23 11:13:24 -0800

    Fixed ttp, added roasts

--- workspace sir vivor/commands.js
+++ workspace sir vivor/commands.js
@@ -1861,7 +1861,7 @@ exports.commands = {
 
 	roast: function (target, user, room) {
 		if (!user.hasRank(room.id, '%')) return;
-		let roasts = [target + 's hair looks like spaghetti', target + '? more like bad', target + ', your presence here is as bad as OM Room\'s theme', target + ', your presence here is bad as Sanjay\'s resignations', target + " is as good as Sanjay's music taste. __You thought this was going to be a roast? Well nope, take this as a compliment :)__"];
+		let roasts = [target + 's hair looks like spaghetti', target + '? more like bad', target + ', your presence here is as bad as __OM Room__\'s theme', target + ', your presence here is bad as Sanjay\'s resignations', target + " is as good as Sanjay's music taste. __You thought this was going to be a roast? Well nope, take this as a compliment :)__", "Your dad is so ugly, he can't even look at himself in the mirror as it breaks at his ugliness!", target + "'s personality is so flat, even flat tires want to run.", target + ", you remind me of gold. You weigh a fuck ton."];
 		this.say(room, Tools.sample(roasts));
 	},
 
