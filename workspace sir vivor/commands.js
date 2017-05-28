d1dceb5598d9d6bfc7a6106b80b53ed162cd4e77
John Shields <johnshields2012@gmail.com>
2017-05-29 00:44:44 +0100

    Allow Leaderboard Searching by Number

--- workspace sir vivor/commands.js
+++ workspace sir vivor/commands.js
@@ -2140,6 +2140,13 @@ exports.commands = {
 	points: function (target, user, room) {
 		if (room.id !== user.id) return;
 		target = Tools.toId(target);
+		if (/^(\-|\+)?([0-9]+|Infinity)$/.test(target)) {
+			target = parseInt(target);
+			let sorted = dd.getSorted();
+			target++;
+			let result = sorted[target];
+			return user.say("**" + result[4] + "** is #" + target + " on the leaderboard with " + dd.getPoints(result) + " points, consisting of " + result[0] + " hosts, " + result[1] + " first places, " + result[2] + " second places, and " + result[3] + " participations.");
+		}
 		if (!target) target = user.id;
 		if (!(target in dd.dd)) {
 			return user.say("**" + target + "** does not have any points.");
