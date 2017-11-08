853c32e4c164e03f3cba75c91c079b99721ad5b1
CheeseMuffin <meesechuffin@gmail.com>
2017-11-07 20:35:36 -0800

    Fix .next

--- workspace sir vivor/commands.js
+++ workspace sir vivor/commands.js
@@ -2037,12 +2037,12 @@ exports.commands = {
 		let millis = (60 - m) * 60 * 1000
 		if (n < 6) {
 			millis += (5 - n) * 60 * 60 * 1000;
-		} else if (n < 16) {
-			millis += (15 - n) * 60 * 60 * 1000;
-		} else if (n < 22) {
-			millis += (21 - n) * 60 * 60 * 1000;
+		} else if (n < 17) {
+			millis += (16 - n) * 60 * 60 * 1000;
+		} else if (n < 23) {
+			millis += (22 - n) * 60 * 60 * 1000;
 		} else {
-			millis += (29 - n) * 60 * 60 * 1000;
+			millis += (30 - n) * 60 * 60 * 1000;
 		}
 		room.say("The next Daily Deathmatch is in " + millisToTime(millis) + ".")
 	},
