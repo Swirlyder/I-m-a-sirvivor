c0ba1527e5b3661a0efea6cce7d543cf734c6e8a
TheMezStrikes <TheMezStrikes@gmail.com>
2019-01-02 18:14:58 +0100

    Rework text commands.

---
+++ workspace sir vivor/commands/text.js
@@ -0,0 +1,131 @@
+/* 
+ * Text-only commands
+ *
+ * Alias syntax: alias: '==originalcommand',
+ */
+
+module.exports = {
+	shame: '==shamethat',
+	shamethat: 'Aussie man of mystery who always beats Beo at RNG',
+	
+	wishes: '==unfixable',
+	unfixable: 'The best eyebrows on PS! and Smogon bar none~',
+
+	usainbot: 'Inferior consumer model',
+
+	bon: '==bondance',
+	bondance: 'Lol, more like can\'t dance',
+	
+	akno: '==aknolan',
+	aknolan: '"I have to go, night!"',
+	
+	woof: 'woof!',
+	
+	swirl: '==swirlyder',
+	swirlyder: 'I swear, he\'s not my real dad', 
+	
+	mitsuki: 'isso significa algo em português', 
+
+	snapeasy: '==snap',
+	snap: '/me snaps fingers',
+	
+	kaz: '==azuuli',
+	azu: '==azuuli',
+	azuuli: '/me explodes',
+
+	lssghost: '==ls',
+	ls: 'try again [user]',
+
+	sciencepanda: '==rach',
+	rach: 'sciencepanda, why do you say that?',
+	
+	unleashourpassion: '==mez',
+	uop: '==mez',
+	mez: 'Consistency is boring.',
+	
+	glacierider: '==glacie',
+	glacie: '/me freezes',
+	
+	alex: '==alexander',
+	alexander489: '==alexander',
+	alexander: '/me left',
+
+	baloor: '__You must be...DELETED!__',
+	
+	cet: '==pak',
+	ceterisparibus: '==pak',
+	pak: 'CHOO CHOO ALL ABOARD THE PARTYBUS TRAIN',
+
+	shadecession: '==shade',
+	shade: 'Better put on my Shadecessions ⌐■_■',
+
+	pq: '==ppq',
+	ppq: '==penquin',
+	penquin: 'S-S-Senpai!',
+	
+	cheese: 'Muffinss',
+	
+	jh: '==teal',
+	seal: '==teal',
+	teal: 'teal teal teaal! >~<',
+	
+	lunar: '==lunarixis',
+	lunarixis: '/me sips tea',
+	
+	spieky: 'Global Roomowner, Administrator and all around Good Guy!',
+	
+	zeo: '==zeonth',
+	zeonth: 'A friendly reminder that Zeonth shamelessly advertises the <<cap>> metagame here',
+	
+	dontlose: '==dl',
+	dl: 'dont lose? HA! More like dont win :^)',
+	
+	zyx14: '==zyx',
+	zyx: 'Famous for .woof and needs to update his number by 3 years',
+	
+	random: '==hawkie',
+	hawkie: 'Which random username will he switch to now?',
+	
+	tikitik: '==tiki',
+	tiki: 'Survivor\'s Resident Russian Spy',
+	
+	soccer: '==soccerz12',
+	soccerz12: 'Hey, what\'s up guys it\'s Soccer here',
+	
+	anime: '==weeb',
+	weeb: 'Anime was a mistake',
+	
+	chapterseven: '==c7',
+	c7: 'Survivor\'s most toxic presence',
+	
+	deathbywobbuffet: '==dbw',
+	wob: '==dbw',
+	dbw: '/me wobs',
+	
+	electra: '==electrasheart',
+	electrasheart: '==summmer',
+	summer: '==moq',
+	moq: 'Don\'t moq me for all my name changes ;_;',
+	
+	morgantactician: 'Emma☯Femcario: Wind Waker, Amiibo Hunter, and  the 5 time Survivor Champion!',
+	
+	pipeitup: '/ME dabs',
+	
+	sanjay: '/ME resigns',
+	
+	micro: '==microwavable',
+	microwavable: '*GGGHHHH*....beep beep bepp....beep beep beep...beep bee-',
+	
+	henka: 'H E N K A B O Y S',
+	
+	zygten: '==zyg',
+	zyg: '/me sighs... what is there to say?',
+	
+	omroom: '==om',
+	om: '"OM Goodness! Leave OM Room alone..." ;~;',
+	
+	tush: '==tushavi',
+	tushavi: '>noob luck',
+	
+	meow: 'meow!',
+}
--- workspace sir vivor/parser.js
+++ workspace sir vivor/parser.js
@@ -12,6 +12,7 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var url = require('url');
+Commands.Replies = require('./commands/text.js');
 
 const ACTION_COOLDOWN = 3 * 1000;
 const FLOOD_MESSAGE_NUM = 7;
@@ -45,264 +46,7 @@ function addChatMessage(message, user) {
 	chatmes[user.id].messages.push(messageAppend);
 }
 
-global.responses = {
-	why: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			usesname: false,
-			before: "Because PenQuin said so.",
-		},
-		{
-			username: false,
-			before: "Why don't you ask later?",
-		},
-		{
-			username: false,
-			before: "Why not?",
-		},
-		{
-			username: false,
-			before: "Blame snap tbh",
-		},
-		],
-	},
-	
-	howmuch: {
-		priority: 4,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "10 gallons worth of water",
-		},
-		{
-			username: true,
-			before: "As many tribesman as ",
-			after: " has",
-		},
-		],
-	},
-	
-	howbad: {
-		priority: 4,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "As bad as anime",
-		},
-		{
-			username: true,
-			before: "As bad as ",
-			after: "",
-		},
-		],
-	},
-	when: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: true,
-			before: "On ",
-			after: "'s birthday!",
-		},
-		{
-			username: false,
-			before: "4/20/2020",
-		},
-		{
-			username: false,
-			before: "today",
-		},
-		{
-			username: false,
-			before: "Tomorrow",
-		},
-		{
-			username: false,
-			before: "This has already happened",
-		},
-		{
-			username: false,
-			before: "When anime is good (aka never)",
-		},
-		{
-			username: false,
-			before: "On April 31st",
-		},
-		],
-	},
-	
-	can: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "No",
-		},
-		{
-			username: false,
-			before: "Yes",
-		},
-		{
-			username: false,
-			before: "Probably not tbh",
-		},
-		{
-			username: false,
-			before: "why would you even ask that",
-		},
-		],
-	},
-
-	am: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "No",
-		},
-		{
-			username: false,
-			before: "Yes",
-		},
-		{
-			username: false,
-			before: "why would you even ask that",
-		},
-		],
-	},
-	should: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "No",
-		},
-		{
-			username: false,
-			before: "Yes",
-		},
-		{
-			username: false,
-			before: "Probably not tbh",
-		},
-		{
-			username: false,
-			before: "obviously not lol",
-		},
-		],
-	},
-	
-	will: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "No",
-		},
-		{
-			username: false,
-			before: "Yes",
-		},
-		{
-			username: false,
-			before: "Probably not tbh",
-		},
-		{
-			username: false,
-			before: "why would you even ask that",
-		},
-		],
-	},
-	where: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "In survivor's plug.",
-		},
-		{
-			username: true,
-			before: "At ",
-			after: "'s house",
-		},
-		{
-			username: false,
-			before: "I'm stuck in botland D:",
-		}
-		],
-	},
-	
-	who: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: true,
-			before: "",
-			after: "",
-		},
-		
-		{
-			username: false,
-			before: "Sir Vivor",
-		},
-		{
-			username: false,
-			before: "Swirlyder",
-		},
-		],
-	},
-	hi: 'hello',
-	hello: {
-		priority: 2,
-		placement: "prefix",
-		responses: [
-		{
-			username: false,
-			before: "Hey <3",
-		},
-		{
-			username: true,
-			before: "Go away ",
-			after: ".",
-		},
-		{
-			username: true,
-			before: "Sup ",
-			after: "",
-		}
-		],
-	},
-	areyou: {
-		priority: 3,
-		placement: "suffix",
-		responses: [
-		{
-			username: false,
-			before: "duh",
-		},
-		{
-			username: false,
-			before: "I hope not",
-		},
-		{
-			username: true,
-			before: "No, but I heard that ",
-			after: " is.",
-		},
-		]
-	},
-}
+global.responses = require('./commands/askvivor.js');
 global.waiting = {};
 function checkHost() {
 	if (Games.hostid) {
@@ -634,6 +378,18 @@ global.parse = exports.parse = {
 				error("invalid command type for " + cmd + ": " + (typeof Commands[cmd]));
 			}
 		}
+        else if (!!Commands.Replies[cmd]) {
+			let failsafe = 0;
+			while (Commands.Replies[cmd].startsWith("==") && failsafe++ < 10) {
+				cmd = Commands.Replies[cmd.substring(2)];
+			}
+            let text = user.hasRank(room.id, '+') ? '' : '/pm ' + user + ', ';
+            text += Commands[cmd];
+            text = text.replace(/\[user\]/g, user);
+            text = text.replace(/\[target\]/g, arg);
+            this.say(room, text);
+            user.lastcmd = cmd;
+        }
 	},
 	say: function (target, text) {
 		if (!target) return;
