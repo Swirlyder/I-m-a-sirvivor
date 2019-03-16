999a9f7a24c848c5b4da1f185a1a88b0af5935a0
1Nactive <elm_10@hotmail.com>
2019-03-16 12:53:14 -0400

    Update rps.js

--- workspace sir vivor/games/rps.js
+++ workspace sir vivor/games/rps.js
@@ -1,6 +1,6 @@
 'use strict';
 const name = "Rock, Paper, Scissors";
-const description = "__Sorry, no lizards or Spocks involved. Winner of NBT #2!__ Game rules: http://survivor-ps.weebly.com/rock-paper-scissors.html **Modification**: includes Lizard and Spock: https://docs.google.com/document/d/1O_6xyXaOnFf6pvBrq3RZW6rFPZBYdO7Q9OIvHervivI/";
+const description = "__Sorry, no lizards or Spocks involved. Winner of NBT #2!__ Game rules: http://survivor-ps.weebly.com/rock-paper-scissors.html **Modification**: includes Lizard and Spock: https://docs.google.com/document/d/e/2PACX-1vR70TQUV1fODj8cGW7r2Vvta5O5pkOXhVsNjw1nVQDWK_8O0YDcnskXrXgXWVnp075OWOHwjxNNZkSL/pub";
 const id = Tools.toId(name);
 class RPS extends Games.Game {
 	constructor(room) {
