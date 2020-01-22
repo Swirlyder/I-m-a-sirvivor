b44920982669e7d956defb3e56ce7174ac3e7e66
TheMezStrikes <=>
2020-01-22 22:20:59 +0100

    fix minefield

--- workspace sir vivor/games/Minefield.js
+++ workspace sir vivor/games/Minefield.js
@@ -94,10 +94,8 @@ class Minefield extends Games.Game {
 				delete this.attacks[toId(this.attacker.name)];
 				return this.handleAttacks();
 			}
-			this.say(`**${this.attacker.name} attacks ${this.defender.name}! Choose your number using \`\`.choose [number (1-3)]\`\`**`);
-			this.phase = true;
-			this.numbers = {};
-			this.timeout = setTimeout(() => this.checkWaiting(), 15 * 1000);
+			this.say(`**${this.attacker.name} attacks ${this.defender.name}!`);
+			this.sayRolls();
 		}
 		else {
 			this.nextRound();
