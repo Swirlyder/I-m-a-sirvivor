986f9458459dcc704fbe5251c13e63d4749430ed
TheMezStrikes <TheMezStrikes@gmail.com>
2020-06-05 01:38:08 +0200

    Don't log chat messages in recv anymore

--- workspace sir vivor/main.js
+++ workspace sir vivor/main.js
@@ -247,7 +247,7 @@ global.connect = function (retry) {
 		con.on('message', function (response) {
 			if (response.type !== 'utf8') return false;
 			var message = response.utf8Data;
-			recv(message);
+			if (toId(message.split('|')[1]) !== 'c') recv(message);
 
 			// SockJS messages sent from the server begin with 'a'
 			// this filters out other SockJS response types (heartbeats in particular)
