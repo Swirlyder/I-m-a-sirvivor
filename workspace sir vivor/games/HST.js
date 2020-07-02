3dddf01b594e0dd6c0c13d02a67f4b33e429ddfe
Shadecession <56569981+Shadecession@users.noreply.github.com>
2020-07-02 00:17:08 -0400

    LC

--- workspace sir vivor/games/HST.js
+++ workspace sir vivor/games/HST.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const name = 'Hide & Seek Tag';
-const description = '__Stop being so damn edgy and just play a childhood game for once.__ Game rules: http://survivor-ps.weebly.com/hide-and-seek-tag.html';
+const description = '__Stop being so damn edgy and just play a childhood game for once.__ Game rules: https://sites.google.com/view/survivor-ps/themes-and-events/survivor-themes/hide-and-seek-tag';
 const id = Tools.toId(name);
 const locations = {
 	bed: {name: "**Under the Bed**", roundname: "Under the Bed", aliases: ["bed", "utb", "underthebed"]},
