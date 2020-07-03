98b6142ca896c9b6bf9b5e7d85637fe9de22317b
Shadecession <56569981+Shadecession@users.noreply.github.com>
2020-07-03 09:32:54 -0400

    n (#137)

--- workspace sir vivor/games/RR.js
+++ workspace sir vivor/games/RR.js
@@ -2,7 +2,7 @@
 
 const name = "Russian Roulette";
 const id = Tools.toId(name);
-const description = '__"Pass like a puss or Pull like a pro."__ Game rules: http://survivor-ps.weebly.com/russian_roulette.html';
+const description = '__"Pass like a puss or Pull like a pro."__ Game rules: https://sites.google.com/view/survivor-ps/themes-and-events/survivor-themes/russian_roulette';
 class RR extends Games.Game {
     constructor(room) {
         super(room);
