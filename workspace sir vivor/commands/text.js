945db68be21b65971aed6739fac1f1520565c6b9
Shadecession <56569981+Shadecession@users.noreply.github.com>
2020-12-05 22:53:43 -0500

    Update text.js

--- workspace sir vivor/commands/text.js
+++ workspace sir vivor/commands/text.js
@@ -5,43 +5,35 @@
  */
 
 module.exports = {
-	shame: '==shamethat',
-	shamethat: 'Aussie man of mystery who always beats Beo at RNG',
-	
-	wishes: '==unfixable',
-	unfixable: 'The best eyebrows on PS! and Smogon bar none~',
 
-	bon: '==bondance',
-	bondance: 'Lol, more like can\'t dance',
-	
-	akno: '==aknolan',
-	aknolan: '"I have to go, night!"',
-	
-	woof: 'woof!',
-	
 	swirl: '==swirlyder',
-	swirlyder: 'I swear, he\'s not my real dad', 
+	swirlyder: 'I swear, he\'s not my real dad',
 	
-	mitsuki: 'isso significa algo em português', 
-
-	snapeasy: '==snap',
-	snap: '/me snaps fingers',
+	pq: '==penquin',
+	ppq: '==penquin',
+	penquin: 'S-S-Senpai!',
 	
-	kaz: '==azuuli',
-	azu: '==azuuli',
-	azuuli: '/me explodes',
-
-	lssghost: '==ls',
-	sothis: '==ls',
-	ls: [
-		"Atone at once!",
-		"Prepare yourself!",
-		"I will cut through!",
-		"Revenge, it lives!"
-	],
-
-	sciencepanda: '==rach',
-	rach: 'The future is now thanks to sciencepanda!',
+	cheese: 'Muffinss',
+	
+	omroom: '==om',
+	om: '"OM Goodness! Leave OM Room alone..." ;~;',
+	
+	shadecession: '==shade',
+	shade: 'Better put on my Shadecessions ⌐■_■',
+	
+	zygten: '==zyg',
+	zyg: '/me sighs... what is there to say?',
+	
+	myster: '==myster17',
+	bugbuzzing: '==myster17',
+	bug: '==myster17',
+	bb: '==myster17',
+	myster17: 'D-don\'t bug me, please... u_u',
+	
+	es: '==epicsnorlax',
+	epicsnorlax: 'good username',
+	eoth: '==easyonthehills',
+	easyonthehills: 'bad name change',
 	
 	unleashourpassion: '==mez',
 	uop: '==mez',
@@ -50,150 +42,27 @@ module.exports = {
 	felu: '==mez',
 	mez: 'Consistency is boring.',
 	
-	glacierider: '==glacie',
-	glacie: 'https://www.reddit.com/r/breadstapledtotrees',
-    
-	guishark: '==gui',
-	gui: 'Hi [target],  I\'m Gui♥Shark!',
-    
-	alex: '==alexander',
-	alexander489: '==alexander',
-	alexander: '/me left',
-
-	baloor: '__You must be...DELETED!__',
-	
-	cet: '==pak',
-	ceterisparibus: '==pak',
-	pak: 'CHOO CHOO ALL ABOARD THE PARTYBUS TRAIN',
-
-	shadecession: '==shade',
-	shade: 'Better put on my Shadecessions ⌐■_■',
-
-	pq: '==penquin',
-	ppq: '==penquin',
-	penquin: 'S-S-Senpai!',
-	
-	cheese: 'Muffinss',
-	
-	jh: '==teal',
-	seal: '==teal',
-	teal: 'teal teal teaal! >~<',
-	
-	lunar: '==lunarixis',
-	lunarixis: '/me sips tea',
-	
-	spieky: 'Global Roomowner, Administrator and all around Good Guy!',
-	
-	zeo: '==zeonth',
-	zeonth: 'A friendly reminder that Zeonth shamelessly advertises the <<cap>> metagame here',
-	
-	dontlose: '==dl',
-	dl: 'dont lose? HA! More like dont win :^)',
-	
-	zyx14: '==zyx',
-	zyx: 'Famous for .woof and needs to update his number by 5 years',
-	
-	tikitik: '==tiki',
-	tiki: 'Survivor\'s Resident Russian Spy',
-	
-	soccer: '==soccerz12',
-	soccerz12: 'Hey, what\'s up guys it\'s Soccer here',
-	
-	anime: '==weeb',
-	weeb: 'Anime was a mistake',
-	
-	chapterseven: '==c7',
-	c7: 'Survivor\'s most toxic presence',
-	
-	deathbywobbuffet: '==dbw',
-	wob: '==dbw',
-	dbw: '/me wobs',
-	
-	electra: '==moq',
-	electrasheart: '==moq',
-	summer: '==moq',
-	moq: 'Don\'t moq me for all my name changes ;_;',
-	
-	morgantactician: 'Emma☯Femcario: Wind Waker, Amiibo Hunter, and  the 5 time Survivor Champion!',
-	
-	pipeitup: '/ME dabs',
-	
-	sanjay: '/ME resigns',
-	
-	microwavable: '*GGGHHHH*....beep beep bepp....beep beep beep...beep bee-',
-	
-	henka: 'H E N K A B O Y S',
-	
-	zygten: '==zyg',
-	zyg: '/me sighs... what is there to say?',
-	
-	omroom: '==om',
-	om: '"OM Goodness! Leave OM Room alone..." ;~;',
-	
-	tush: '==tushavi',
-	tushavi: '>noob luck',
-	
-	inac: '==inactive',
-	inactive: 'My Art is an explosion!!',
-
-	meow: 'meow!',
-	
-	sips: '==sip',
-	sip: '/me sips [target]\'s tea',
-	
-	dn: '==deeznuts',
-	deeznuts: '[target]? More like deez nuts :^)',
-	
 	gim: '==gimm1ck',
 	gimmick: '==gimm1ck',
 	gimm1ck: '/me squats',
 	
-	toni: '==tonixy',
-	tonixy: 'free quilava flow',
+	inac: '==inactive',
+	inactive: 'My Art is an explosion!!',
 	
-	wenis: '==elbowskinn',
-	elbowskinn: '!dt linoone',
-	
-	ptoad: 'teehee',
-	
-	bobo: '==bobochan',
-	bobochan: '妮可妮可妮[target]，波波酱在此！',
-	
-	workshop: '<<survivorworkshop>>',
-	
-	geene: '/me spins',
-	
-	myster: '==myster17',
-	bugbuzzing: '==myster17',
-	bug: '==myster17',
-	bb: '==myster17',
-	myster17: 'D-don\'t bug me, please... u_u',
-	
-	egg: '[target] has been egged!',
-	
-	toss: 'The egg was passed on to [target].',
-	
-	explode: 'BOOOOOOOOOOOOOM THE EGG EXPLODED ON [target] !!! HAHAHA GET EGGED', 
-	
-	dywy: '胖吉胖吉胖吉猫',
-	
-	pisxel: 'LOL cringe',
-	pix: '==pisxel',
-	
-	profsapling: '==prof',
-	sapling: '==prof',
-	prof: 'Welcome [target] to the Survivor region! Are you a boy, or a girl?',
-	
-	hasumikisaragi: 'hasumi',
-	hasumi: 'Next Stop: きさらぎ鉄道駅',
+	mega: '==megagr',
+	gr: '==megagr',
+	megagr: 'GRRRRRRR',
 	
 	micro: '==micromorphic',
 	micromorphic: 'what',
 	
-	es: '==epicsnorlax',
-	epicsnorlax: 'good username',
-	eoth: '==easyonthehills',
-	easyonthehills: 'bad name change',
+	mit: '==mitsuki',
+	mitsuki: 'isso significa algo em português',
+	
+	tush: '==tushavi',
+	tushavi: '>noob luck',
+	
+	ptoad: 'teehee',
 	
 	abd1710: [
 		"Survivor Album song 1: https://www.youtube.com/watch?v=9M_FlkAiTSo",
@@ -202,18 +71,81 @@ module.exports = {
 	],
 	abd: '==abd1710',
 	
-	mrtoastd: '==toast',
-	toast: '/me toasts to [target]',
+	bea: '==beautifications',
+	beautifications: 'Survivor Queenpin',
+	
+	bon: '==bondance',
+	bondance: 'Lol, more like can\'t dance',
+	
+	guishark: '==gui',
+	gui: 'Hi [target],  I\'m Gui♥Shark!',
 	
 	centi: '==idybalajrenyt',
 	idybalajrenyt: 'GOOOOOOOOOOOLAZO DE DYBALA in my YT Channel Dybala Jr (?)',
 	
-	bea: '==beautifications',
-	beautifications: 'Survivor Queenpin',
+	orion: 'hasumi',
+	hasumikisaragi: 'hasumi',
+	hasumi: 'Next Stop: きさらぎ鉄道駅',
 	
-	mega: '==megagr',
-	gr: '==megagr',
-	megagr: 'GRRRRRRR',
+	henka: 'H E N K A B O Y S',
+	
+	mrtoastd: '==toast',
+	toast: '/me toasts to [target]',
+	
+	profsapling: '==prof',
+	sapling: '==prof',
+	prof: 'Welcome [target] to the Survivor region! Are you a boy, or a girl?',
+	
+	spieky: 'Global Roomowner, Administrator and all around Good Guy!',
+	
+	soccer: '==soccerz12',
+	soccerz12: 'Hey, what\'s up guys it\'s Soccer here',
+	
+	sanjay: '/ME resigns',
+	
+	snapeasy: '==snap',
+	snap: '/me snaps fingers',
+	
+	tikitik: '==tiki',
+	tiki: 'Survivor\'s Resident Russian Spy',
+	
+	zeo: '==zeonth',
+	zeonth: 'A friendly reminder that Zeonth shamelessly advertises the <<cap>> metagame here',
+	
+	bobo: '==bobochan',
+	bobochan: '妮可妮可妮[target]，波波酱在此！',
+	
+	dywy: '胖吉胖吉胖吉猫',
+    
+	alex: '==alexander',
+	alexander489: '==alexander',
+	alexander: '/me left',
+	
+	pisxel: 'LOL cringe',
+	pix: '==pisxel',
+	
+	anime: '==weeb',
+	weeb: 'Anime was a mistake',
+	
+	dn: '==deeznuts',
+	deeznuts: '[target]? More like deez nuts :^)',
+	
+	meow: 'meow!',
+	
+	pipeitup: '/ME dabs',
+	
+	sips: '==sip',
+	sip: '/me sips [target]\'s tea',
+	
+	woof: 'woof!',
+	
+	egg: '[target] has been egged!',
+	
+	toss: 'The egg was passed on to [target].',
+	
+	explode: 'BOOOOOOOOOOOOOM THE EGG EXPLODED ON [target] !!! HAHAHA GET EGGED',
+	
+	workshop: '<<survivorworkshop>>',
 	
 }
 	
