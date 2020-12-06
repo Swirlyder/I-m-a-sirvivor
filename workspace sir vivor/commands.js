3119a97083fc9b1255459af8638dd9de2b8256c0
Shadecession <56569981+Shadecession@users.noreply.github.com>
2020-12-06 16:48:32 -0500

    more para command users

--- workspace sir vivor/commands.js
+++ workspace sir vivor/commands.js
@@ -1189,17 +1189,20 @@ let commands = {
 	paradise: 'para',
 	para: function (arg, user, room) {
 		let text1 = 'I\'m Paradise and this is my Anime club. I work here with my friends and fellow weebs: Spieky, Bon Dance, Don’t Lose, Aknolan, PenQuin, Swirlyder, Aknolan, Moo, Snap, Hawkie';
-		let text2 = 'Toni XY, Henka, OM, Zeonth, Zyx14, phable, deetah, Hurl, Abd1710, cleo, Ls\'s Ghost, ptoad, Rainshaft, pants, wob, Ceteris, Gimmick, Shadecession, Rach, Harambeween';
-		let text3 = 'geene, inactive, mitsuki, Tushavi, Zyg, Santa Vivor, Guishark, TheBluestEye, ProfSapling, hasumi kisaragi, Megagr, iDybalaJRenYT!~, and Calem DPU; and in 23 years, I\'ve learned one thing. You never know WHAT anime is going to be good.';
+		let text2 = 'Toni XY, Henka, OM, Zeonth, Zyx14, Shadecession, phable, deetah, Hurl, abd1710, cleo, LS\'s Ghost, ptoad, Rainshaft, pants, wob, Ceteris, Gimmick, Rach, Harambeween';
+		let text3 = 'Geene, inactive, Mitsuki, Tushavi, Zyg, Santa Vivor, Guishark, TheBluestEye, BugBuzzing, EasyOnTheHills, Felucia, micromorphic, Megagr, Penguin D'; 
+		let text4 = 'ProfSapling, hasumi kisaragi, iDybalaJRenYT!~, ClaudioINK58, and beautifications, and in 23 years, I\'ve learned one thing. You never know WHAT anime is going to be good.';
 		if (room !== user && !user.hasRank(room, '+')) {
 			user.say(text1);
 			user.say(text2);
 			user.say(text3);
+			user.say(text4);
 
 		} else {
 			room.say(text1);
 			room.say(text2);
 			room.say(text3);
+			room.say(text4);
 		}
 	},
 	
