9a7baffe4780947c9f0c9739215bb458ad7f1d50
1Nactive <elm_10@hotmail.com>
2023-12-29 17:42:44 -0500

    Update commands.js

--- workspace sir vivor/commands.js
+++ workspace sir vivor/commands.js
@@ -1380,9 +1380,9 @@ let commands = {
 	paradise: 'para',
 	para: function (arg, user, room) {
 		let text1 = 'I\'m Paradise and this is my Anime club. I work here with my friends and fellow weebs: Spieky, Bon Dance, Don’t Lose, Aknolan, PenQuin, Swirlyder, Aknolan, Moo,';
-		let text2 = 'Snap, Hawkie, Henka, OM, Zeonth, Zyx14, Shadecession, deetah, Hurl, zyg, Guishark, Mitsuki, Tushavi, abd1710, cleo, ptoad, Rainshaft, pants, wob, Ceteris,';
-		let text3 = 'Gimmick, Rach, Harambeween, inactive, TheBluestEye, BugBuzzing, EasyOnTheHills, Felucia, micromorphic, Megagr, Penguin D, ClaudioINK58, lfolfo, confix,'; 
-		let text4 = 'beautifications, 2guhde4u, and Gary The Savage, and in 23 years, I\'ve learned one thing. You never know WHAT anime is going to be good.';
+		let text2 = 'Snap, Hawkie, Henka, OM, Zeonth, Zyx14, Shadecession, deetah, Hurl, zyg, Guishark, Mitsuki, Tushavi, inactive, cleo, ptoad, Rainshaft, pants, wob, Ceteris,';
+		let text3 = 'Gimmick, Harambeween, abd1710, TheBluestEye, BugBuzzing, EasyOnTheHills, Felucia, micromorphic, Megagr, Penguin D, ClaudioINK58, lfolfo, corjon,'; 
+		let text4 = 'beautifications, 2guhde4u, Gary The Savage, Pacific Marill, Opple, Le\'Depression, SaltiestCactus43, Sificon, and crabachillable and in 23 years, I\'ve learned one thing. You never know WHAT anime is going to be good.';
 		if (room !== user && !user.hasRank(room, '+')) {
 			user.say(text1);
 			user.say(text2);
