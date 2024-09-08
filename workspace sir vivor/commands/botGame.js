d251dcd361ac0aa97230858fdf49a9b3f7d0ffb5
Swirlyder <tristian.schaub@gmail.com>
2024-09-07 21:37:56 -0400

    Update botGame.js

--- workspace sir vivor/commands/botGame.js
+++ workspace sir vivor/commands/botGame.js
@@ -213,7 +213,7 @@ module.exports = {
 						if (!newPlayer) return this.say(room, "User not found.");
 						Games.addPlayer(newPlayer);
 
-						if(Games.playerListToolEnabled && user.rooms.has('survivor')) {
+						if(Games.playerListToolEnabled && Users.get(Games.host.id)) {
 							const plhtml = PL_Menu.generatePLAssistantHTML();
 							Parse.say(room, '/sendhtmlpage ' + Games.host.id + ', Playerlist-Assistant, ' + plhtml); 
 						}
