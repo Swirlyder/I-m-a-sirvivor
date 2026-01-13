c96e8492b15134383a1793f7cd76191082df8306
1Nactive <elm_10@hotmail.com>
2026-01-13 18:05:48 -0500

    Update botHelp.js

--- workspace sir vivor/commands/botHelp.js
+++ workspace sir vivor/commands/botHelp.js
@@ -16,12 +16,9 @@ module.exports = {
 	about: function (arg, user, room) {
 		user.say(`I am a bot made for the Survivor room. Please contact Survivor room auth for any questions regarding me!`)
 	},
-
-	help: 'commands',
-	guide: 'commands',
-	commands: function (arg, user, room) {
+	guide: function (arg, user, room) {
 		let target = user.hasRank(room, '+') ? room : user;
-		let text = "The guide for my commands is here: https://docs.google.com/document/d/e/2PACX-1vSkPg4Wao_p7WB2q1FIrBZuRYydluHgg0OYoC3sDoooWvy6IqOdQ5zn3-SjrSfKz60RQm33M9Ekbqzj/pub";
-		room.say(text);
+		let text = "Command guide: https://docs.google.com/document/d/e/2PACX-1vSkPg4Wao_p7WB2q1FIrBZuRYydluHgg0OYoC3sDoooWvy6IqOdQ5zn3-SjrSfKz60RQm33M9Ekbqzj/pub";
+		target.say(text);
 	},
 };
